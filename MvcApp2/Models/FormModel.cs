08e76e751e743f96ff7bc8218866da9ae560a343
adamfed7 <91409021+adamfed7@users.noreply.github.com>
2021-11-15 16:21:42 +0100

    Add project files.

---
+++ MvcApp2/Models/FormModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcApp2.Models
+{
+    public class FormModel
+    {
+        public string Name { get; set; }
+        public int iloscPowtorzen { get; set; }
+
+    }
+
+}
+
